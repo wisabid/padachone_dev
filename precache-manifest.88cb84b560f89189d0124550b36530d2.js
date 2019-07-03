77d0525fcc3856eac0cddd92a3a6810f62b57ef0
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-07-04 00:01:39 +0200

    Updates

---
+++ precache-manifest.88cb84b560f89189d0124550b36530d2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f23f2272f57a0fc0c08fc1e683d9c4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59db2db1503d571f4d5d",
+    "url": "/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "e1ee7a241b56ed0655bc",
+    "url": "/static/css/main.8e07ab6c.chunk.css"
+  },
+  {
+    "revision": "59db2db1503d571f4d5d",
+    "url": "/static/js/2.8e16099f.chunk.js"
+  },
+  {
+    "revision": "e1ee7a241b56ed0655bc",
+    "url": "/static/js/main.090888d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd3c65418ff56e0c4c4b0fd6f7b24e1a",
+    "url": "/static/media/Prayer-time.dd3c6541.jpg"
+  },
+  {
+    "revision": "d6d84b1951d54e118344294b83805881",
+    "url": "/static/media/bg-new.d6d84b19.png"
+  },
+  {
+    "revision": "0b13e3dc6652d1471ee99ec56c4a5d67",
+    "url": "/static/media/boo.0b13e3dc.svg"
+  },
+  {
+    "revision": "c1619cce189e6edecd5eebd6cecd3461",
+    "url": "/static/media/cool.c1619cce.svg"
+  },
+  {
+    "revision": "599f38ab6e02ba5ef809c750b1fc9c22",
+    "url": "/static/media/goofy.599f38ab.svg"
+  },
+  {
+    "revision": "8be94c129f8e59d1ecfce1ff36c2e014",
+    "url": "/static/media/logo-sec.8be94c12.png"
+  },
+  {
+    "revision": "a510d8e26fb933d8ccd03aa2d2f46db5",
+    "url": "/static/media/quietTime.a510d8e2.mp3"
+  }
+]);
